f7b9527a32ef2f6837ddcbc2dd5d210602053ffb
senayasfaha <sasfaha@iu.edu>
2024-12-08 18:36:10 -0500

    fixing up

--- src/Config/dbConnection.ts
+++ src/Config/dbConnection.ts
@@ -4,25 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = new Pool({
-    host: "asme-db.cn2o0ao8aj7i.us-east-2.rds.amazonaws.com",
-    user: "asmedb",
-    password: "asmedbdb",
-    database: "asme_intial",
-    port: 5432,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT) || 5432
 });
-
-// GITHUB_TOKEN=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
-// OPENAI_API_KEY=sk-Ew9D6Nly-l7jX-TBKyOAGWnD3alcsZPSLPODi-2RtRT3BlbkFJet5HHHqauJ58_46iZGKeXVffgRo4sWq6OCwsTkprgA
-// DB_USER=asmedb
-// DB_HOST=asme-ds.cn2o0ao8aj7i.us-east-2.rds.amazonaws.com
-// DB_NAME=asme_intial
-// DB_PASSWORD=asmedbdb
-// DB_PORT=5432
-// DB_SSL=false
-// PORT= 8080
 
 pool.on('connect', () => {
     console.log('Database connected successfully');
